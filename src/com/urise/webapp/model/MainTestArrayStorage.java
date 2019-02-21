526005bbb26ffdb9e2b9834f7077b4024a43b8c9
pavelb89 <pavelb73@gmail.com>
2019-02-21 17:43:08 +0300

    HW2 - 2

--- src/com/urise/webapp/model/MainTestArrayStorage.java
+++ src/com/urise/webapp/model/MainTestArrayStorage.java
@@ -1,16 +1,17 @@
+package com.urise.webapp.model;
+
+import com.urise.webapp.storage.*;
+
 /**
- * Test for your ArrayStorage implementation
+ * Test for your com.urise.webapp.storage.ArrayStorage implementation
  */
 public class MainTestArrayStorage {
     static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
-        Resume r1 = new Resume();
-        r1.uuid = "uuid1";
-        Resume r2 = new Resume();
-        r2.uuid = "uuid2";
-        Resume r3 = new Resume();
-        r3.uuid = "uuid3";
+        Resume r1 = new Resume("uuid1");
+        Resume r2 = new Resume("uuid2");
+        Resume r3 = new Resume("uuid3");
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
